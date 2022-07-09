7895a3c5acb60310c7911a3bd742bef1d7f78c63
thiagopazBR <98658387+thiagopazBR@users.noreply.github.com>
2022-07-08 22:26:44 -0300

    Update read_csv.ts

--- src/functions/read_csv.ts
+++ src/functions/read_csv.ts
@@ -1,17 +1,17 @@
+import parser from 'csv-parser'
 import { createReadStream } from 'fs'
 
 type ICsvData = {
-  [key: string]: any
+  [key: string]: string
 }
 
 async function read_csv(csv_file_path: string): Promise<ICsvData[]> {
   const data: ICsvData[] = []
   return new Promise((resolve, reject) => {
     createReadStream(csv_file_path)
-      .pipe(csvtojson())
-      .on('data', function (d) {
-        const jsonStr = d.toString('utf8')
-        data.push(jsonStr)
+      .pipe(parser())
+      .on('data', function (row) {
+        data.push(row)
       })
       .on('end', function () {
         resolve(data)
